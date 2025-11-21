929088e9a0ac9b56749466ec9d523a42eaad3b95
aishuaishwaryab641-dot <aishuaishwaryab641@gmail.com>
2025-11-21 11:52:20 +0530

    Add files via upload

---
+++ struct_demo.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+struct student
+{
+    char name[100];
+    int age;
+    float marks;
+};
+
+void main()
+{
+
+struct student s1;
+strcpy(s1.name, "joey");
+s1.age = 19;
+s1.marks = 89.6;
+
+struct student s2;
+strcpy(s2.name, "Raj");
+s2.age = 20;
+s2.marks = 90.6;
+strcpy(s2.name, "Raj");
+
+struct student s3 = {"Popoye",17,99.9};
+
+printf("%s",s3.name);
+printf("%d",s3.age);
+}
